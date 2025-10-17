e848226bffe7871c1fde400517d7602adb8c185a
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-17 23:28:13 +0300

    Init commit.

---
+++ src/main/java/ru/kuznetsov/shop/data_model_dto/DataModelDtoApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.data_model_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataModelDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataModelDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsov/shop/data_model_dto/DataModelDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.data_model_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataModelDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
