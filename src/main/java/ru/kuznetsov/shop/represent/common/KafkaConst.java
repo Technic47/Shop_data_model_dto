c3587951c7e432f2744f9bd5b538da12118e74e9
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-18 17:21:38 +0300

    Kafka topics added.

---
+++ src/main/java/ru/kuznetsov/shop/represent/common/KafkaConst.java
@@ -0,0 +1,34 @@
+package ru.kuznetsov.shop.represent.common;
+
+public interface KafkaConst {
+    //Save
+    String ADDRESS_SAVE_TOPIC = "shop_save_address";
+    String PRODUCT_CATEGORY_SAVE_TOPIC = "shop_save_product_category";
+    String PRODUCT_SAVE_TOPIC = "shop_save_product";
+    String STOCK_SAVE_TOPIC = "shop_save_stock";
+    String STORE_SAVE_TOPIC = "shop_save_store";
+
+    //Save successful
+    String ADDRESS_SAVE_SUCCESSFUL_TOPIC = "shop_save_address_successful";
+    String PRODUCT_CATEGORY_SAVE_SUCCESSFUL_TOPIC = "shop_save_product_category_successful";
+    String PRODUCT_SAVE_SUCCESSFUL_TOPIC = "shop_save_product_successful";
+    String STOCK_SAVE_SUCCESSFUL_TOPIC = "shop_save_stock_successful";
+    String STORE_SAVE_SUCCESSFUL_TOPIC = "shop_save_store_successful";
+
+    //Save failed
+    String ADDRESS_SAVE_FAIL_TOPIC = "shop_save_address_fail";
+    String PRODUCT_CATEGORY_SAVE_FAIL_TOPIC = "shop_save_product_category_fail";
+    String PRODUCT_SAVE_FAIL_TOPIC = "shop_save_product_fail";
+    String STOCK_SAVE_FAIL_TOPIC = "shop_save_stock_fail";
+    String STORE_SAVE_FAIL_TOPIC = "shop_save_store_fail";
+
+    //Update
+    String ADDRESS_UPDATE_TOPIC = "shop_update_address";
+    String PRODUCT_CATEGORY_UPDATE_TOPIC = "shop_update_product_category";
+    String PRODUCT_UPDATE_TOPIC = "shop_update_product";
+    String STOCK_UPDATE_TOPIC = "shop_update_stock";
+    String STORE_UPDATE_TOPIC = "shop_update_store";
+
+    //Headers
+    String OPERATION_ID_HEADER = "operation_id";
+}
